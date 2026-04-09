cc0a8a4d4e5ecb43fdc2f1fc31a99e8253a2af69
Mohamed Mansour <hello@mohamedmansour.com>
2026-04-08 19:16:36 -0700

    doc: fix scroll to the top when hitting next

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,8 +2,18 @@
 // Licensed under the MIT license.
 
 import DefaultTheme from 'vitepress/theme'
+import { useRoute } from 'vitepress'
+import { watch, nextTick } from 'vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
+  setup() {
+    const route = useRoute()
+    watch(() => route.path, () => {
+      nextTick(() => {
+        document.querySelector('#VPContent')?.scrollTo({ top: 0 })
+      })
+    })
+  },
 }
